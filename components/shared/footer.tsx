da8c5ee4cf51eb7561062c5e2f1c4849ba9b885a
Maksym Azimov <anthoniotheruiner@gmail.com>
2024-06-24 01:06:38 +0200

    fix: add surname to the portfolio  owner

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="pb-10 px-4 text-center text-gray-500 dark:bg-darkBg">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Maksym. All rights reserved.
+        &copy; 2024 Maksym Azimov. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">
