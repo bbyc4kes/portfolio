0f2f77fe07213e2a5280d74743461a80a4c15b09
Maksym Azimov <anthoniotheruiner@gmail.com>
2024-08-04 13:57:13 +0200

    fix: header active section stiffness bug

--- src/common/components/shared/header/index.tsx
+++ src/common/components/shared/header/index.tsx
@@ -44,7 +44,7 @@ export default function Header() {
                     layoutId="activeSection"
                     transition={{
                       type: 'spring',
-                      stiffness: 380,
+                      stiffness: 300,
                       damping: 30,
                     }}
                   ></motion.span>
