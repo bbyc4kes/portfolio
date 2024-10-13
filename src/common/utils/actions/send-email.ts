152c6a9010e664e0136f371c8ab39fe470cf26f1
Chandima Ekanayake <emcc1990@gmail.com>
2024-10-14 02:33:45 +0800

    email updated

--- src/common/utils/actions/send-email.ts
+++ src/common/utils/actions/send-email.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: ["anthoniotheruiner@gmail.com"],
+      to: ["emcc1990@gmail.com"],
       subject: "Message from contact form | PORTFOLIO",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
